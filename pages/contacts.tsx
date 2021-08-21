d86a32ae21675016fc6d369c8a6b749e59a573a1
v <vreshch@gmail.com>
2021-08-21 23:44:36 +0200

    Correct type

--- pages/contacts.tsx
+++ pages/contacts.tsx
@@ -18,7 +18,7 @@ export default function ContactsPage() {
                     <p>Email: vreshch@gmail.com</p>
                     <p>Skype: vreshch.work</p>
                     <p>Tel: +38(096)337-87-**</p>
-                    <p>Linkden: <a href="https://www.linkedin.com/in/vreshch-volodymyr-3969498a" target="_blank">Volodymyr Vreshch</a></p>
+                    <p>Linkedin: <a href="https://www.linkedin.com/in/vreshch-volodymyr-3969498a" target="_blank">Volodymyr Vreshch</a></p>
                     <p>Github: <a href="https://github.com/vreshch" target="_blank">vreshch</a></p>
 
                 </div>
